5c1754c47df923c687a7fd894a2d7faaa94d9b0c
AMeDhat <bodey146@gmail.com>
2025-05-09 02:42:18 +0300

    add error module ,handle regular errors and validation error

--- SpeechCorrection.APIs/Errors/ApiResponse.cs
+++ SpeechCorrection.APIs/Errors/ApiResponse.cs
@@ -1,7 +1,28 @@
-﻿namespace SpeechCorrection.APIs.Errors
+﻿
+namespace SpeechCorrection.APIs.Errors
 {
     public class ApiResponse
     {
-        
+        public int StatusCode { get; set; } 
+
+        public string? Message { get; set; }
+
+        public ApiResponse(int statusCode, string? message = null)
+        {
+            StatusCode = statusCode;
+            Message = message ?? GetDefaultMessageForStatusCode(statusCode);
+        }
+
+        private string? GetDefaultMessageForStatusCode(int statusCode)
+        {
+            return statusCode switch
+            {
+                400 => "Bad Request",
+                401 => "Unauthorized",
+                404 => "Not Found",
+                500 => "Internal Server Error",
+                _ => "Error? What error?"
+            };
+        }
     }
 }
--- SpeechCorrection.APIs/Program.cs
+++ SpeechCorrection.APIs/Program.cs
@@ -1,23 +1,39 @@
+using Microsoft.AspNetCore.Mvc;
+using SpeechCorrection.APIs.Errors;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+#region ConfigueServices 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
 builder.Services.AddOpenApi();
+builder.Services.Configure<ApiBehaviorOptions>(options =>
+{
+	options.InvalidModelStateResponseFactory = (context) =>
+	{
+		var errors = context.ModelState.Where(e => e.Value.Errors.Count > 0).SelectMany(e => e.Value.Errors)
+			.Select(e => e.ErrorMessage).ToList();
+		return new BadRequestObjectResult(new ApiValidationErrorResponse() { Errors = errors });
+	};
+});
+#endregion
 
 var app = builder.Build();
 
+#region MiddleWares
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+	app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
-app.MapControllers();
+app.MapControllers(); 
+#endregion
 
 app.Run();
